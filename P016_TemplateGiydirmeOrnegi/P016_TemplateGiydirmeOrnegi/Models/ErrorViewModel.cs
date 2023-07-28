aac4603eec1d23709eecf18beffab83ad5d336a4
ayktoytn <oytun.aykut@gmail.com>
2023-07-28 23:52:30 +0300

    The partial view structure of the website was created, the pages were redirected. Layout and master structures were created, article structures were created for different content.

---
+++ P016_TemplateGiydirmeOrnegi/P016_TemplateGiydirmeOrnegi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P016_TemplateGiydirmeOrnegi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ResultTurleri/ResultTurleri/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResultTurleri.Models;
+using System;
+using System.Diagnostics;
+
+namespace ResultTurleri.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Mesaj = "Result türümüz bir view";
+            return View();
+        }
+        public PartialViewResult Index2()
+        {
+            // sayfa içerisindeki js kodlarını parçalamak için kullanırız. 
+            //PartialViewResult result = new PartialViewResult();
+            PartialViewResult result = PartialView(); // bunu kullanmamızın nedeni ana sayfanın js kod yükünü almak. Metot oluşturur gibi bu kısımlarda da belli küçük kod bütünleri oluşturur ve lazım olunca çağırırız.
+            return result;
+        }
+
+        public JsonResult GetAllPerson()
+        {
+            JsonResult data = Json(
+                new Person { Name = "Selami", Surname = "Şahin" },
+                new Person { Name = "Bülent", Surname = "Ersoy" }
+
+                );
+            return data;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
